539f575832244e426d768b0901113a1e45b25f3f
Wang Xiaozhe <chaoslawful@gmail.com>
2011-04-07 15:53:30 +0800

    modify python ext setup script

--- src/python_zfor/setup.py
+++ src/python_zfor/setup.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 from distutils.core import setup, Extension
 
-zformod = Extension('zfor',
-                          sources = ['src/zfor.c'],  library_dirs = ['/usr/local/lib'], libraries = ['zfor']
-                          )
+zformod = Extension(
+            'zfor',
+            sources = ['src/zfor.c'],
+            include_dirs = ['../libzfor'],
+            library_dirs = ['/usr/local/lib', '../libzfor'],
+            libraries = ['zfor']
+        )
 
-setup(name = 'zfor',
-      version = '0.1',
-      description = 'Python zfor binding',
-      author = ['Chris Goffinet'],
-      author_email = ['cg@chrisgoffinet.com'],
-      packages = [
-        'zfor',
-      ],
-      package_dir = {'zfor' : 'src'},
-      ext_modules = [zformod],
-      )
+setup(
+        name = 'zfor',
+        version = '0.1',
+        description = 'Python zfor binding',
+        author = ['Chris Goffinet'],
+        author_email = ['cg@chrisgoffinet.com'],
+        packages = [
+            'zfor',
+            ],
+        package_dir = {'zfor' : 'src'},
+        ext_modules = [zformod],
+    )
+
+# vim:ft=python ts=4 sw=4 et
+
